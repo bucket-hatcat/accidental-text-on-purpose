f0c43f2237795cc73bfa1839158c1df5d6d3891f
Lauren Wierzbicki <lauren@laurenwierzbicki.com>
2021-11-06 16:15:21 -0700

    please

---
+++ public/src/components/App/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Home from '../Home/Home';
+import {Route, Link, Switch, Redirect} from 'react-router-dom'
+import './App.css';
+import Currencies from '../Currencies/Currencies';
+import Currency from '../Currency/Currency';
+
+
+function App() {
+  const [currency, setCurrency] = useState('');
+
+  return (
+    <div>
+      <nav>
+        <Link to='/'><img src="https://en.bitcoin.it/w/images/en/2/29/BC_Logo_.png"alt=""/>
+          <h1>Bitcoin prices</h1>
+          </Link>
+        <Link to="/currencies/list"> Currencies </Link>
+      </nav>
+      <main>
+          {/* <Home /> */}
+      <switch>
+        <Route path='/' exact component={Home}/>
+        <Route path= '/homepage' exact render={() => <Redirect to="/" /> }/>
+        <Route path='/currencies/list'  exact component={Currencies}/>
+        <Route path='/price/:currencySymbol'  exact render={(routerProps) => <Currency match={routerProps.match} />} />    
+      </switch>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ public/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App/App.js";
+import { BrowserRouter as Router } from "react-router-dom";
+
+// anytime a component wraps around another component...its called
+// as Higher Order Component
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);

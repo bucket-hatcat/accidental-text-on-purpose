0c63fd1501af5e9c62e1a45fb6546593f5d5c6a0
Lauren Wierzbicki <lauren@laurenwierzbicki.com>
2021-11-11 11:48:34 -0800

    Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect, submitForm } from "react";
-import { useNavigate, Router } from "react-router";
+import Navbar from "./Components/Header.js";
 import "./App.css";
-import {Nav, Navbar, Container } from "react-bootstrap";
-import {LinkContainer} from 'react-router-bootstrap'
-
-
+import About from "./Components/About";
+import { render } from "@testing-library/react";
+// import {LinkContainer} from 'react-router-bootstrap'
 function App() {
   const [advice, setAdvice] = useState("");
   // https://api.adviceslip.com/advice
-
   useEffect(() => {
     fetch("https://api.adviceslip.com/advice")
       .then((res) => res.json())
@@ -17,7 +15,6 @@ function App() {
         // console.log("LOAD ADVICE", advice);
       });
   }, []);
-
   let fetchNewAdvice = () => {
     fetch("https://api.adviceslip.com/advice")
       .then((res) => res.json())
@@ -26,57 +23,21 @@ function App() {
         // console.log("BUTTON ADVICE", advice)
       });
   };
-
-  let navigate = useNavigate();
-  let [error, setError] = React.useState(null);
-
-  const header = () => {
-    return (
-
-function handleSubmit(event) {
-    event.preventDefault();
-    let result = submitForm(event.target);
-    if (result.error) {
-      setError(result.error);
-    } else {
-      navigate("success");
-    }
-  })
-
+  
   return (
     <div className="App">
-    
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <LinkContainer>
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-  </LinkContainer>
-</Navbar>
+<Navbar />
+</div>
 
 
-
-      
-      <div className="advice">
-        <h2>{advice}</h2>
-      </div>
-      <br />
-      <button className="btn" onClick={fetchNewAdvice}>
-        Generate New advice
-      </button>
-    </div>
+    //   <div className="advice">
+    //     <h2>{advice}</h2>
+    //   </div>
+    //   <br />
+    //   <button className="btn" onClick={fetchNewAdvice}>
+    //     Generate New advice
+    //   </button>
+    // </div>
   );
-}}
-
+}
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Navbar} from 'react-bootstrap';
+import {NavItem} from 'react-bootstrap';
+import {Nav} from 'react-bootstrap';
+
+function Header() {
+    return (
+        <div>
+          <h1>SFPOPOS</h1>
+        </div>
+      )
+    }
+
+export default Header;
